4db9e111f255c5fe6a180e301c261ff3519039f5
abhinav2304 <44238766+abhinav2304@users.noreply.github.com>
2023-11-16 13:28:10 +0000

    update

--- web-main-corrected/summarize.py
+++ web-main-corrected/summarize.py
@@ -1,25 +1,26 @@
 import openai
 
-def summarize_questions(api_key, file_path='data/questions.txt'):
+# Directly assign your actual API key here
+api_key = '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'
+
+client = openai.OpenAI(api_key=api_key)
+
+def summarize_questions(file_path='data/questions.txt'):
     with open(file_path, 'r', encoding='utf-8') as file:
         file_content = file.read()
 
-    prompt_text = "The following are questions from a lecture. Summarize them into bullet points:\n\n" + file_content
+    prompt_text = "The following are questions from students from a lecture. correct the sentence formation if needed. translate to english if there are any questions from other languages.Summarize them into bullet points:\n\n" + file_content
 
-    openai.api_key = api_key
-
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo-1106",
-        prompt=prompt_text,
-        max_tokens=100,  # Reduced for conciseness
-        n=1,
-        stop=None,
-        temperature=0.0,
-        frequency_penalty=0.5  # Added to reduce repetition
-    )
+    # Correcting the API call by using 'model' instead of 'engine'
+    response = client.completions.create(model="gpt-3.5-turbo-instruct", prompt=prompt_text,
+                                         max_tokens=100,  # Reduced for conciseness
+                                         n=1,
+                                         stop=None,
+                                         temperature=0.0,
+                                         frequency_penalty=1)
 
     return response.choices[0].text.strip()
 
-api_key = ''  # Replace with your actual API key
-summary = summarize_questions(api_key)
+# Call the function and print the summary
+summary = summarize_questions()
 print(summary)
